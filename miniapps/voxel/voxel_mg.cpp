e6dff2e95192f98e973ef886f4d131f813537726
Will Pazner <will.e.p@gmail.com>
2023-12-08 22:56:50 -0800

    Voxel Poisson problem

--- miniapps/voxel/voxel_mg.cpp
+++ miniapps/voxel/voxel_mg.cpp
@@ -1,5 +1,6 @@
 #include "mfem.hpp"
 #include "voxel_mesh.hpp"
+#include "mg.hpp"
 
 using namespace std;
 using namespace mfem;
@@ -17,26 +18,95 @@ int main(int argc, char *argv[])
    args.AddOption(&order, "-o", "--order", "Finite element polynomial degree");
    args.ParseCheck();
 
-   H1_FECollection fec(order, 2);
-   VoxelMesh mesh(mesh_file, h);
+   H1_FECollection fec(order, 3);
 
+   VoxelMultigrid mg(VoxelMesh(mesh_file, h), fec);
+   FiniteElementSpace &fespace = mg.GetFineSpace();
+   const int nl = mg.nlevels;
+
+   ConstantCoefficient one(1.0);
+   LinearForm b(&fespace);
+   b.AddDomainIntegrator(new DomainLFIntegrator(one));
+   b.Assemble();
+
+   GridFunction x(&fespace);
+   x = 0.0;
+
+   OperatorHandle A;
+   Vector B, X;
+   mg.FormFineLinearSystem(x, b, A, X, B);
+
+   PCG(*A, mg, B, X, 1, 200, 1e-12, 0.0);
+
+   mg.GetFineForm().RecoverFEMSolution(X, b, x);
+
+   ParaViewDataCollection pv("VoxelMG", fespace.GetMesh());
+   pv.SetPrefixPath("ParaView");
+   pv.SetHighOrderOutput(true);
+   pv.SetLevelsOfDetail(order + 1);
+   pv.RegisterField("u", &x);
+   pv.SetCycle(0);
+   pv.SetTime(0);
+   pv.Save();
+
+   Vector x_prev = x;
+   for (int i = 0; i < nl - 1; ++i)
+   {
+      FiniteElementSpace &space = *mg.spaces[nl-2-i];
+      GridFunction x_c(&space);
+      mg.prolongations[nl-2-i]->Coarsen(x_prev, x_c);
+      x_prev = x_c;
+
+      pv.SetMesh(space.GetMesh());
+      pv.RegisterField("u", &x_c);
+      pv.SetCycle(i+1);
+      pv.SetTime(i+1);
+      pv.Save();
+   }
+
+#if 0
+
+   VoxelMesh mesh(mesh_file, h);
    VoxelMesh coarsened_mesh = mesh.Coarsen();
 
-   coarsened_mesh.Save("coarsened_voxel_mesh.mesh");
+   FiniteElementSpace fine_fes(&mesh, &fec);
+   FiniteElementSpace coarse_fes(&coarsened_mesh, &fec);
+
+   GridFunction fine_gf(&fine_fes);
+   GridFunction coarse_gf(&coarse_fes);
+
+   auto fn = [](const Vector &xvec)
+   {
+      const double x = xvec[0];
+      const double y = xvec[1];
+      return exp(0.1*sin(5.1*x - 6.2*y) + 0.3*cos(4.3*x +3.4*y));
+   };
+   FunctionCoefficient fn_coeff(fn);
+   coarse_gf.ProjectCoefficient(fn_coeff);
+
+   Array<int> empty;
+   VoxelProlongation P(coarse_fes, empty, fine_fes, empty);
+   P.Mult(coarse_gf, fine_gf);
+
+   // coarsened_mesh.Save("coarsened_voxel_mesh.mesh");
 
    ParaViewDataCollection pv("VoxelMG", &mesh);
    pv.SetPrefixPath("ParaView");
    pv.SetHighOrderOutput(true);
    pv.SetLevelsOfDetail(order + 1);
 
+   pv.RegisterField("u", &fine_gf);
    pv.SetCycle(0);
    pv.SetTime(0);
    pv.Save();
 
    pv.SetMesh(&coarsened_mesh);
+   pv.RegisterField("u", &coarse_gf);
    pv.SetCycle(1);
    pv.SetTime(1);
    pv.Save();
+
+#endif
 
    return 0;
 }
